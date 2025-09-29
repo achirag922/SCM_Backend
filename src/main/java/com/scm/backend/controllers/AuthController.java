565fd11e0d927c73c7be590718cd179af86ccb88
ChiragAgarwal <achirag922@gmail.com>
2025-09-29 14:39:21 +0530

    final keyword used in authController class

--- src/main/java/com/scm/backend/controllers/AuthController.java
+++ src/main/java/com/scm/backend/controllers/AuthController.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,9 +22,9 @@ import java.util.Optional;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private AuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
     private UserDetailsService userDetailsService;
-    private JwtService jwtService;
+    private final JwtService jwtService;
     private final UserRepo userRepo;
 
     private AuthController(AuthenticationManager authenticationManager, UserDetailsService userDetailsService, JwtService jwtService, UserRepo userRepo) {
--- src/main/java/com/scm/backend/entities/event/UserEventHandler.java
+++ src/main/java/com/scm/backend/entities/event/UserEventHandler.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
